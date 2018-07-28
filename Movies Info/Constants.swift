74a2c3cb5137ae942641c11461d6ccda7804913e
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2018-07-28 16:04:52 +0100

    initial commit

---
+++ Movies Info/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Movies Info
+//
+//  Created by Rasheed Sulayman on 28/07/2018.
+//  Copyright © 2018 r4sh33d. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "5018815e8ba8370cd1ca9f94183aba1a"
+}
