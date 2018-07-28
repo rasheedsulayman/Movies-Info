bfbd15f238ba329284864304231b3f80d57b42de
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2018-07-28 18:30:54 +0100

    Add Tmdb API base URL and Movies Posters Base URL

--- Movies Info/Constants.swift
+++ Movies Info/Constants.swift
@@ -10,4 +10,6 @@ import Foundation
 
 struct Constants {
     static let API_KEY = "5018815e8ba8370cd1ca9f94183aba1a"
+    static let BASE_URL = "https://api.themoviedb.org/3/movie/"
+    static let IMAGES_BASE_URL = "https://image.tmdb.org/t/p/"
 }
