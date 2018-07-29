d40920dcefaa7079ab4fe0a0b083f203cddc6ff0
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2018-07-29 15:20:12 +0100

    Add more Base URL related constants

--- Movies Info/Constants.swift
+++ Movies Info/Constants.swift
@@ -12,4 +12,6 @@ struct Constants {
     static let API_KEY = "5018815e8ba8370cd1ca9f94183aba1a"
     static let BASE_URL = "https://api.themoviedb.org/3/movie/"
     static let IMAGES_BASE_URL = "https://image.tmdb.org/t/p/"
+    static let BACK_DROP_BASE_URL = IMAGES_BASE_URL + "/w500"
+    static let POSTER_BASE_URL = IMAGES_BASE_URL + "/w185"
 }
---
+++ Movies Info/MovieGridViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridViewCell.swift
+//  Movies Info
+//
+//  Created by Rasheed Sulayman on 29/07/2018.
+//  Copyright © 2018 r4sh33d. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var ratingsLabel: UILabel!
+}
